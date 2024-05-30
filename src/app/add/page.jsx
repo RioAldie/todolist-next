ff8c9bcffb416eb280f69d741c7a92cc3ba768ae
rioaldie <rioaldierwanto@gmail.com>
2024-05-30 10:46:18 +0700

    make a add task page design

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -3,8 +3,19 @@ import styles from '@/components/add/add.module.css';
 const AddTask = () => {
   return (
     <main className={styles.main}>
-      <form action=""></form>
-      <p>This Add Page</p>
+      <div className={styles.section}>
+        <form className={styles.container}>
+          <h1>Add New Task</h1>
+          <input type="text" name="title" placeholder="title" />
+          <textarea
+            type="text"
+            name="body"
+            placeholder="task"
+            rows={10}
+          />
+          <button className={styles.save}>Save</button>
+        </form>
+      </div>
     </main>
   );
 };
